47b154c1d86f5d1ba284ce276f8de95e8f19ff2a
Cristiano <cftrajano@live.com>
2020-10-27 22:51:21 -0300

    back-end do usuario completo

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -24,3 +24,80 @@ exports.Insert = (req, res, next) =>{
     })
     .catch(error => next(error));
 };
+
+exports.SelectAll = (req, res, next) => {
+    Usuario.findAll()
+        .then(usuario => {
+            if(usuario){
+                res.status(status.OK).send(usuario);
+            }
+        })
+    .catch(error => next(error));
+}
+
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+
+    Usuario.findByPk(id)
+        .then(usuario =>{
+            if(usuario){
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const cpf = req.body.cpf;
+    const email = req.body.email;
+    const senha = req.body.senha;
+    const ativo = req.body.ativo;
+
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.update({
+                    nome: nome,
+                    cpf: cpf,
+                    email: email,
+                    senha: senha,
+                    ativo: ativo,
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+    .catch(error => next(error));
+};
